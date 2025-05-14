a2738c9c971b8258b303d30b9c7c4355c25dbd9f
newweb <newweb@laposte.net>
2025-05-14 14:54:07 +0200

    boucle for avec un while pour test, attention voir i et taille (valeur) pour avoir une sortie exacte

--- 2-for/code.js
+++ 2-for/code.js
@@ -1,1 +1,14 @@
-console.log("ok");
+// console.log("ok");
+
+const container = document.querySelector(".container");
+const phrase = "je suis dieu !";
+
+let i = -1;
+let taille = phrase.length-1;
+while (i < taille) {
+    const p = document.createElement("p");
+    container.appendChild(p);
+    // console.log(phrase[i]);
+    i++;
+    p.textContent=`${phrase[i]}`
+}
