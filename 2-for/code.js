1a08a5819ec5c615582005eface7ea987aaadae8
newweb <newweb@laposte.net>
2025-05-15 18:44:22 +0200

    boucle for et for of ok

--- 2-for/code.js
+++ 2-for/code.js
@@ -1,14 +1,79 @@
 // console.log("ok");
 
 const container = document.querySelector(".container");
-const phrase = "je suis dieu !";
+// const phrase = "je suis dieu !";
 
-let i = -1;
-let taille = phrase.length-1;
-while (i < taille) {
-    const p = document.createElement("p");
-    container.appendChild(p);
-    // console.log(phrase[i]);
-    i++;
-    p.textContent=`${phrase[i]}`
+// let i = -1;
+// let taille = phrase.length-1;
+// while (i < taille) {
+//     const p = document.createElement("p");
+//     container.appendChild(p);
+//     // console.log(phrase[i]);
+//     i++;
+//     p.textContent=`${phrase[i]}`
+// }
+
+// for (let i = 0; i < phrase.length; i++) {
+//     const p = document.createElement("p");
+//     container.appendChild(p);
+//     p.textContent = `${phrase[i]}`
+//     console.log(phrase[i]);
+
+// }
+
+// afficher des nombres paire
+// for (let i = 0; i <= 20; i++) {
+//     const p = document.createElement("p");
+//     container.appendChild(p);
+
+//     if (i%2 === 0) {
+//         p.textContent = `${i}`
+//         console.log(i);
+//     }
+// }
+
+// calculer des puissances
+// const nb = 2;
+// const puissance = 15
+// let result = 1;
+
+// for (let i = 0; i < puissance; i++)
+// {
+//     const p = document.createElement("p");
+//     container.appendChild(p);
+//     p.textContent = `${result = result * nb} -  `;
+//     console.log(i);
+
+// }
+
+// trouver la valeur la plus grande dans un tableau
+// const tab = [2, -6, 19, 35, 41, 28, -25, -7, 9, 24, 18, 89, -56];
+// let max = tab[0];
+// for (let i = 1; i < tab.length; i++) {
+//   if (tab[i] > max) max = tab[i];
+// }
+// const p = document.createElement("p");
+// container.appendChild(p);
+// p.textContent = `${max}`;
+
+// calcul moyen d'un éleve
+const notes = [[8,9,14], [15,14,13], [10,17]];
+let result = 0;
+let nbNotes = 0;
+let moyenne;
+for(let i = 0; i < notes.length; i++){
+    for(let y = 0; y < notes[i].length; y++){
+        // console.log(notes[i][y]);
+        result += notes[i][y];
+        nbNotes++;
+        // console.log(result);
+        // console.log(nbNotes);
+        moyenne = result/nbNotes;
+        console.log(moyenne);
+    }
 }
+
+const p = document.createElement("p");
+container.appendChild(p);
+// p.textContent = `${Math.round(moyenne)}`;
+p.textContent = `${moyenne.toFixed(2)}`;
--- 3-for-of/code.js
+++ 3-for-of/code.js
@@ -1,1 +1,68 @@
-console.log("ok");
+// console.log("ok");
+
+
+
+// const phrase = "Je ne suis pas un dieu en code !";
+// for(let i = 0; i<phrase.length; i++){
+//     console.log(phrase[i]);
+    
+// }
+
+// avec for of 
+// for(let lettre of phrase){
+//     console.log(lettre);
+    
+// }
+
+// const languages = ["Java", "C#", "JS", "PHP"];
+// for(let language of languages)
+// {
+//     console.log(language);
+    
+// }
+
+// const temps = [8,6,14,26,41,18,32,17,23,25];
+// let maxTmps = temps[0];
+// let minTmps = temps[0];
+// for(let temp of temps) {
+//     if(temp > maxTmps){
+//         maxTmps = temp;
+//     }
+// }
+// console.log(maxTmps);
+// for(let temp of temps) {
+//     if(temp < minTmps){
+//         minTmps = temp;
+//     }
+// }
+// console.log(minTmps);
+
+
+// const notes = [[8,9,14], [15,11,13], [10,17]];
+// let result = 0;
+// let nbNotes = 0;
+// let moyenne;
+// for(let tabNote of notes){
+//     for(let value of tabNote){
+//         result += value;
+//         nbNotes++;
+//         moyenne = result/nbNotes;
+
+//     }
+// }
+
+// console.log(result);
+// console.log(moyenne.toFixed(2));
+
+const lis = document.querySelectorAll("li");
+const ul = document.querySelector("ol");
+// console.log(lis.length);
+for(li of lis){
+    li.style.color = "LightSeaGreen";
+    li.style.backgroundColor = "DarkSlateGray";
+    li.style.padding = "5px 10px";
+}
+ul.style.padding = "50px" ;
+ul.style.margin = "50px" ;
+ul.style.backgroundColor = "DarkOrange";
+
